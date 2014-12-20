585ed7ed024b93ae2703a077c639ef505c211a30
Piotr Kedra <piotr@kraksoft.pl>
2014-12-20 23:19:32 +0100

    Change box naming rule

--- build-debian.sh
+++ build-debian.sh
@@ -3,7 +3,7 @@
 # Configurations
 BUILD_OSNAME="debian"
 BUILD_OSTYPE="Debian_64"
-BOX="debian-wheezy-64"
+BOX="debian-7.7.0-amd64"
 ISO_URL="http://cdimage.debian.org/mirror/cdimage/release/7.7.0/amd64/iso-cd/debian-7.7.0-amd64-netinst.iso"
 ISO_MD5="0b31bccccb048d20b551f70830bb7ad0"
 #ISO_URL="http://cdimage.debian.org/mirror/cdimage/archive/7.6.0/amd64/iso-cd/debian-7.6.0-amd64-netinst.iso"
--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -3,7 +3,7 @@
 # Configurations
 BUILD_OSNAME="ubuntu"
 BUILD_OSTYPE="Ubuntu_64"
-BOX="ubuntu-trusty-64"
+BOX="ubuntu-14.10-amd64"
 ISO_URL="http://releases.ubuntu.com/14.10/ubuntu-14.10-server-amd64.iso"
 ISO_MD5="91bd1cfba65417bfa04567e4f64b5c55"
 #ISO_URL="http://releases.ubuntu.com/14.04/ubuntu-14.04-server-amd64.iso"
