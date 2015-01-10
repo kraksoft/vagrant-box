806e4da7b93ff64ad45e35dbef3217fb153ae03e
Piotr Kedra <piotr@kraksoft.pl>
2015-01-10 18:10:41 +0100

    Temporary revert old versions for tagging

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -3,10 +3,8 @@
 # Configurations
 BUILD_OSNAME="ubuntu"
 BUILD_OSTYPE="Ubuntu_64"
-BOX="ubuntu-14.10-amd64"
-ISO_URL="http://releases.ubuntu.com/14.10/ubuntu-14.10-server-amd64.iso"
-ISO_MD5="91bd1cfba65417bfa04567e4f64b5c55"
-#ISO_URL="http://releases.ubuntu.com/14.04/ubuntu-14.04-server-amd64.iso"
-#ISO_MD5="01545fa976c8367b4f0d59169ac4866c"
+BOX="ubuntu-14.04-amd64"
+ISO_URL="http://releases.ubuntu.com/14.04/ubuntu-14.04-server-amd64.iso"
+ISO_MD5="01545fa976c8367b4f0d59169ac4866c"
 
 ./build.sh ${BUILD_OSNAME} ${BUILD_OSTYPE} ${BOX} ${ISO_URL} ${ISO_MD5}
