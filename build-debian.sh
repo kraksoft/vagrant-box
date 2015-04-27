9f911b9d43fb3e537f2249366ff304790fa0a096
Piotr Kedra <piotr@kraksoft.pl>
2015-04-27 23:51:32 +0200

    Change url for version 8.0.0 to unofficial Debian mirror

--- build-debian.sh
+++ build-debian.sh
@@ -4,8 +4,8 @@ BUILD_OSNAME="debian"
 BUILD_OSTYPE="Debian_64"
 
 # last 'STABLE' version
-BOX="debian-7.8.0-amd64"
-ISO_URL="http://mirror.i3d.net/pub/debian-cd/7.8.0/amd64/iso-cd/debian-7.8.0-amd64-netinst.iso"
-ISO_MD5="a91fba5001cf0fbccb44a7ae38c63b6e"
+BOX="debian-8.0.0-amd64"
+ISO_URL="http://cdimage.debian.org/debian-cd/8.0.0/amd64/iso-cd/debian-8.0.0-amd64-netinst.iso"
+ISO_MD5="d9209f355449fe13db3963571b1f52d4"
 
 ./build.sh ${BUILD_OSNAME} ${BUILD_OSTYPE} ${BOX} ${ISO_URL} ${ISO_MD5}
