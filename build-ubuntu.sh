bc4c05b27b8d68f375fe09c9cfa9ceaaadb03f4d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-04-27 15:31:32 +0200

    Update URL for 14.04 ISO

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -5,7 +5,7 @@ BUILD_OSTYPE="Ubuntu_64"
 
 # last 'LTS' version
 BOX="ubuntu-14.04-amd64"
-ISO_URL="http://releases.ubuntu.com/14.04/ubuntu-14.04-server-amd64.iso"
+ISO_URL="http://ubuntu-release.locaweb.com.br/14.04/ubuntu-14.04-server-amd64.iso"
 ISO_MD5="01545fa976c8367b4f0d59169ac4866c"
 
 ./build.sh ${BUILD_OSNAME} ${BUILD_OSTYPE} ${BOX} ${ISO_URL} ${ISO_MD5}
