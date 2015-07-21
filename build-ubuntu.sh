fe69cb47fd66af0b6c802314ff91d13777d332f3
Piotr Kedra <piotr.kedra@schibsted.pl>
2015-07-21 16:31:32 +0200

    Update Ubuntu parameters

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -3,8 +3,8 @@
 BUILD_OSNAME="ubuntu"
 BUILD_OSTYPE="Ubuntu_64"
 
-BOX="ubuntu-15.04-amd64"
-ISO_URL="http://releases.ubuntu.com/releases/15.04/ubuntu-15.04-server-amd64.iso"
-ISO_MD5="487f4a81f22f8597503db3d51a1b502e"
+BOX="ubuntu-14.10-amd64"
+ISO_URL="http://releases.ubuntu.com/releases/14.10/ubuntu-14.10-server-amd64.iso"
+ISO_MD5="91bd1cfba65417bfa04567e4f64b5c55"
 
 ./build.sh ${BUILD_OSNAME} ${BUILD_OSTYPE} ${BOX} ${ISO_URL} ${ISO_MD5}
