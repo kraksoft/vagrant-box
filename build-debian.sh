b76ac490e9820550ab9072669c893b768bc73a30
Piotr Kedra <piotr.kedra@schibsted.pl>
2015-07-21 20:37:25 +0200

    Fix Debian parameters

--- build-debian.sh
+++ build-debian.sh
@@ -4,7 +4,7 @@ BUILD_OSNAME="debian"
 BUILD_OSTYPE="Debian_64"
 
 BOX="debian-8.0.0-amd64"
-http://debian.apt-get.eu/cd-images/8.0.0/amd64/iso-cd/debian-8.0.0-amd64-netinst.iso
+ISO_URL="http://debian.apt-get.eu/cd-images/8.0.0/amd64/iso-cd/debian-8.0.0-amd64-netinst.iso"
 ISO_MD5="d9209f355449fe13db3963571b1f52d4"
 
 ./build.sh ${BUILD_OSNAME} ${BUILD_OSTYPE} ${BOX} ${ISO_URL} ${ISO_MD5}
