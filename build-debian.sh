2795f7658f8152e2fca9bc9c3d97424d6667a894
Piotr Kedra <piotr.kedra@schibsted.pl>
2015-07-21 20:00:39 +0200

    Update Debian parameters

--- build-debian.sh
+++ build-debian.sh
@@ -3,8 +3,8 @@
 BUILD_OSNAME="debian"
 BUILD_OSTYPE="Debian_64"
 
-BOX="debian-8.1.0-amd64"
-ISO_URL="http://cdimage.debian.org/debian-cd/8.1.0/amd64/iso-cd/debian-8.1.0-amd64-netinst.iso"
-ISO_MD5="1a311f9afb68d6365211b13b4342c40b"
+BOX="debian-7.8.0-amd64"
+ISO_URL="http://debian.apt-get.eu/cd-images/7.8.0/amd64/iso-cd/debian-7.8.0-amd64-netinst.iso"
+ISO_MD5="a91fba5001cf0fbccb44a7ae38c63b6e"
 
 ./build.sh ${BUILD_OSNAME} ${BUILD_OSTYPE} ${BOX} ${ISO_URL} ${ISO_MD5}
